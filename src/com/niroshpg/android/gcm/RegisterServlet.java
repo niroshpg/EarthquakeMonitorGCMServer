233e9fb14cb4d15af80dd524b5cf5d18fc43177a
niroshpg <niroshpg@gmail.com>
2016-01-19 02:52:19 +1100

    processing incoming json data for registration

--- src/com/niroshpg/android/gcm/RegisterServlet.java
+++ src/com/niroshpg/android/gcm/RegisterServlet.java
@@ -15,11 +15,17 @@
  */
 package com.niroshpg.android.gcm;
 
+import java.io.BufferedReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 /**
  * Servlet that registers a device, whose registration id is identified by
@@ -41,6 +47,49 @@ public class RegisterServlet extends BaseServlet {
 	  logger.warning(" start registering doPost");
     String regId = getParameter(req, PARAMETER_REG_ID);
     logger.warning(" start registering " + regId);
+    
+    
+    StringBuffer jb = new StringBuffer();
+    logger.warning(" start registering : sb created" );
+    String line = null;
+    try {
+      BufferedReader reader = req.getReader();
+      logger.warning(" start registering : reader created" );
+      while ((line = reader.readLine()) != null)
+        jb.append(line);
+    } catch (Exception e) { /*report an error*/ }
+
+    logger.warning(" start registering : jb append completed" );
+    JSONObject jsonObject = null;
+      try {
+    	  String encodeStr = new String(jb.toString().getBytes(), "UTF-8");
+    	  logger.warning(" start registering : encodeStr = " + encodeStr );
+    	  String decodeStr = URLDecoder.decode(encodeStr,"UTF-8");
+    	  logger.warning(" start registering : decodeStr = " + decodeStr );
+		 jsonObject = new JSONObject(decodeStr);
+		 logger.warning(" start registering : from json regId = " + jsonObject.getString("regId"));
+	} catch (JSONException e) {
+		// TODO Auto-generated catch block
+		 logger.warning(" start registering :json error : " + e.getMessage() );
+		e.printStackTrace();
+	} catch (UnsupportedEncodingException e) {
+		 logger.warning(" start registering :encoding error" );
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  
+      if(jsonObject == null)
+      {
+    	  logger.warning(" start registering : could not read json data");
+      }
+
+    // Work with the data using methods like...
+    // int someInt = jsonObject.getInt("intParamName");
+     // String someString = jsonObject.getString("stringParamName");
+    // JSONObject nestedObj = jsonObject.getJSONObject("nestedObjName");
+    // JSONArray arr = jsonObject.getJSONArray("arrayParamName");
+    // etc...
+    
     Datastore.register(regId);    
     setSuccess(resp);
   }
